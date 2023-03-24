bc780a8bc9b70e845e3bd675afc926fb73d5c664
ianfshirley <ianfshirley88@gmail.com>
2023-03-24 09:14:13 -0700

    added method_label to jobs/serializers.py

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -3,6 +3,11 @@ from .models import Job
 
 
 class JobSerializer(serializers.ModelSerializer):
+    method_label = serializers.CharField(source='get_method_display', read_only=True)
+
     class Meta:
         model = Job
         fields = "__all__"
+
+        # Add the method_label to the list of fields
+        fields = list(fields) + ['method_label']
