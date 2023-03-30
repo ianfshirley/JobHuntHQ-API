0aa964fa93f1e93c1314252f305196435b05be23
ianfshirley <ianfshirley88@gmail.com>
2023-03-30 13:55:41 -0700

    added deployed front end site to cors allowed origins

--- job_hunt_api/settings.py
+++ job_hunt_api/settings.py
@@ -144,7 +144,12 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://job-hunt-hq.vercel.app/",
+]
+
 
 AUTH_USER_MODEL = "accounts.User"
 
